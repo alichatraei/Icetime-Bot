0a86dc3623e975d3342bc0691889c324aca35ffc
avi9406 <avida.chatraee2001@yahoo.com>
2021-01-06 10:19:29 +0330

    Create Project

---
+++ bot.js
@@ -0,0 +1,5 @@
+const telegraf = require("telegraf");
+const axios = require("axios");
+const bot = new telegraf("1569361684:AAGhYhSQiBY24zji00F-UcaQJKKX9I195Y4");
+// get URL from User
+bot.launch();
