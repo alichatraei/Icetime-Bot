0df542e8363a17c86af90c21b02bd5bbccd2e9ff
avi9406 <avida.chatraee2001@yahoo.com>
2021-01-07 16:44:09 +0330

    Completed Project

--- bot.js
+++ bot.js
@@ -1,5 +1,39 @@
 const telegraf = require("telegraf");
 const axios = require("axios");
-const bot = new telegraf("1569361684:AAGhYhSQiBY24zji00F-UcaQJKKX9I195Y4");
+const bot = new telegraf("1545033877:AAER-07UtJrNoZ5SbUibEA7QutixZDxLlyQ");
 // get URL from User
+
+
+bot.command('tell', async (ctx) => {
+    try {
+        console.log();
+        const content = await genJoke();
+        bot.telegram.sendMessage(ctx.chat.id, content)
+    } catch (err) {
+        console.log(err);
+    }
+
+})
+//functions
+async function genJoke() {
+    let joke;
+    await axios.request({
+        method: 'GET',
+        url: 'https://joke3.p.rapidapi.com/v1/joke',
+        headers: {
+            'x-rapidapi-key': 'e6b6dc7894msh3e5fba24f14c7a4p10d06djsn31c3e0bcdab5',
+            'x-rapidapi-host': 'joke3.p.rapidapi.com'
+        }
+    }).then(res => {
+        let {
+            data: {
+                content
+            }
+        } = res;
+        joke = content;
+    }).catch(err => {
+        return err;
+    })
+    return joke;
+}
 bot.launch();
